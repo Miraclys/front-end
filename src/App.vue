915d3bd2f6e50db7d2594c75cf8af6eeabf2bfcc
Miraclys <2496438098@qq.com>
2023-03-11 23:20:20 +0800

    3-11-23-20

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <template>
+  <el-container>
+    <el-aside style="width:180px;">
+    </el-aside>
+    <el-container>
+      <el-header>XX管理系统</el-header>
+      <el-main>内容区域</el-main>
+      <el-footer>版权所有</el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+</template>
+
+<style scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+.el-menu {
+  background-color: #d3dce6;
+}
+</style>
+
+<style>
+      html,body,#app{
+        height:100%;
+        margin: 0px;
+        padding: 0px;
+      }
+    </style>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
