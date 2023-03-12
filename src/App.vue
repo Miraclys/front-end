beb293c8f711d341b7775338f3493ed55998b498
Miraclys <2496438098@qq.com>
2023-03-12 13:13:27 +0800

    3-12-13-14

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="common-layout">
+  <div class="chat-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header class="header">
+        Mini-chatGPT
+      </el-header>
       <el-container>
-        <el-aside width="100px">
-          Aside
+        <el-aside width="200px" class="aside">
+          在线用户列表
         </el-aside>
 
         <el-container>
           <el-main>
-            Main
+            <div class="chat-window">
+              <div class="chat-bubble" v-for="(message, index) in messages" :key="index">
+                <div class="message-info">{{message.username}} {{message.time}}</div>
+                <div class="message-content">{{message.content}}</div>
+              </div>
+            </div>
           </el-main>
 
-          <el-footer>
-            Footer
+          <el-footer class="footer">
+            <el-input v-model="input" placeholder="请输入消息" clearable size="medium" style="height: 50px; flex: 1;"/>
+            <el-button class="send-button" @click="sendMessage">发送</el-button>
           </el-footer>
 
         </el-container>
@@ -27,34 +35,113 @@
     height: 100%;
   }
 
-  .common-layout {
+  .chat-layout {
     height: 100vh;
+    display: flex;
+    flex-direction: column;
   }
 
   .el-container {
     height: 100%;
   }
 
-  .el-header,
-  .el-footer {
-    background-color: #b3c0d1; color: #333; text-align: center; line-height: 60px;
+  .header {
+    background-color: #007bff; 
+    color: #fff; 
+    text-align: center; 
+    line-height: 60px;
   }
 
-  .el-aside {
-    background-color: #d3dce6; color: #333; text-align: center; line-height: 200px;
+  .aside {
+    background-color: #f0f0f0; 
+    color: #333; 
+    text-align: center; 
+    line-height: 200px;
   }
 
   .el-main {
-    background-color: #e9eef3; color: #333; text-align: center; line-height: 160px;
+    background-color: #fff; 
+    color: #333; 
+    padding: 20px;
+    overflow-y: auto;
   }
 
-  .el-menu {
-    background-color: #d3dce6;
+  .chat-window {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .chat-bubble {
+    margin: 10px 0;
+    max-width: 60%;
+    align-self: flex-start;
+    border-radius: 5px;
+    padding: 10px;
+    background-color: #f0f0f0;
+  }
+
+  .chat-bubble:nth-child(even) {
+    align-self: flex-end;
+    background-color: #007bff;
+    color: #fff;
+  }
+
+  .message-info {
+    font-size: 12px;
+    color: #999;
+    margin-bottom: 5px;
+  }
+
+  .message-content {
+    font-size: 14px;
+  }
+
+  .footer {
+    display: flex;
+    align-items: center;
+    background-color: #f0f0f0;
+    padding: 10px;
+  }
+
+  .send-button {
+    margin-left: 10px;
   }
 </style>
 
 <script>
   export default {
-    name: "App", 
+
+    sendMessage() {
+      const message = this.input;
+      const time = new Date().toLocaleTimeString();
+      const newRecord = { time, content: message };
+      this.records.push(newRecord);
+      console.log(`发送消息：${message}，时间：${time}`);
+      this.input = '';
+    },
+
+    name: "ChatRoom", 
+    data() {
+      return {
+        input: "",
+        messages: [
+          {
+            username: "Alice",
+            time: "2022-01-01 10:00:00",
+            content: "你好"
+          },
+          {
+            username: "Bob",
+            time: "2022-01-01 10:01:00",
+            content: "你好，有什么需要帮助的吗？"
+          },
+          {
+            username: "Alice",
+            time: "2022-01-01 10:02:00",
+          }
+        ]
+      }
+    }
   }
 </script>
+           
